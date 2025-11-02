a13003571af191180f37c3ff4177010522e1c298
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-11-02 18:47:15 -0300

    subo semana 34 y 35

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 32',
-    data: [10, 12, 17, 13, 26], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 34',
+    data: [17, 15, 19, 12,25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 33',
-    data: [15, 23, 16, 20, 28], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 35',
+    data: [10, 13, 10, 10, 15], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
